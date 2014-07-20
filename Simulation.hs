5ad6623ef655995a387dbb8824bae5223960dcfc
Roy Tu <thatsnail@gmail.com>
2014-07-20 17:04:27 -0400

    Added positron

--- Simulation.hs
+++ Simulation.hs
@@ -39,5 +39,8 @@ testSimElectron = Simulation "Single Electron" [Particle Electron (Vector3D 0 0 
 testSimTwoElectrons :: Simulation
 testSimTwoElectrons = Simulation "Two Electrons" [Particle Electron (Vector3D (-0.5) 0 0) (Vector3D 0 0 0), Particle Electron (Vector3D 0.5 0 0) (Vector3D 0 0 0)] 0.001 10 10
 
+testSimElectronPositron :: Simulation
+testSimElectronPositron = Simulation "Electron and Positron" [Particle Electron (Vector3D (-0.5) 0 0) (Vector3D 0 0 0), Particle Positron (Vector3D 0.5 0 0) (Vector3D 0 0 0)] 0.001 10 10
+
 testSimHydrogen :: Simulation
 testSimHydrogen = Simulation "Single Hydrogen Atom" (spawnAtom Hydrogen (Vector3D 0 0 0) (Vector3D 0 0 0) []) 0.001 10 10
