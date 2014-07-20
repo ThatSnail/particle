57a68dc7f9f95e9dcec8391953492fbc75d6a41e
Roy Tu <thatsnail@gmail.com>
2014-07-20 02:00:40 -0400

    Major GUI additions, basic drawing and simulation animation

--- Gui.hs
+++ Gui.hs
@@ -1,9 +1,34 @@
 import Graphics.Gloss
 
+import GHC.Float
+import Simulation
+import Particle
+import Math
+import Algorithm
+
 w :: Int
 w = 500
 
 h :: Int
 h = 500
 
-main = display (InWindow "Particle Simulator" (w, h) (10, 10)) white (Circle 80)
+buildAnimation :: [(Double, [Particle])] -> Float -> Picture
+buildAnimation frames t = getPicture maybeInd
+    where
+        ts = map fst frames
+        ps = map snd frames
+        maybeInd = binarySearchLower (float2Double t) ts
+        getPicture (Just i) = drawParticles (ps !! i)
+        getPicture Nothing = blank
+
+drawParticles :: [Particle] -> Picture
+drawParticles ps = pictures $ map drawParticle ps
+
+drawParticle :: Particle -> Picture
+drawParticle p@(Particle pType (Vector3D x y z) _) = translate (double2Float x) (double2Float y) $ color red $ circleSolid 10
+
+drawSimulation :: [(Double, [Particle])] -> IO ()
+drawSimulation simOut = animate (InWindow "Particle Simulator" (w, h) (10, 10)) black $ buildAnimation simOut
+
+main :: IO ()
+main = drawSimulation $ runSimulation $ testSimHydrogen
